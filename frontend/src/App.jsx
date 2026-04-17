5d76e3c1cfbc37ea116e00d88567fbfcf6668c65
Suchith K <Suchith3000@gamil.com>
2026-04-17 11:05:22 +0530

    doctor-hero app and homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
+import Homepage from "./pages/homepage";
+
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-100">
-      <h1 className="text-4xl font-bold text-blue-600 underline">
-        Tailwind is Working!
-      </h1>
-    </div>
-  );
+  return <Homepage />;
 }
 
 export default App;
