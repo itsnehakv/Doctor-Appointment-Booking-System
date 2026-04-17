b6634faf81ab7e7cb36e03234d36ff91b465dcec
Suchith K <Suchith3000@gamil.com>
2026-04-17 13:39:43 +0530

    feat: add Footer, Testimonials

--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -1,11 +1,15 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Footer from "../components/footer";
+import Testimonials from "../components/review";
 
 function HomePage() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Testimonials/>
+      <Footer/>
     </>
   );
 }
